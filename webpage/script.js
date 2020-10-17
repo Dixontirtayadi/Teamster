99ead9f28215a57998348cba13cf2d4d826a3f4e
Shawn Cho <sycho00@cs.washington.edu>
2020-10-17 14:22:02 -0700

    test html

---
+++ webpage/script.js
@@ -0,0 +1,10 @@
+"use strict"
+(function() {
+    window.addEventListener('load', init);
+
+    function init() {
+      var name = document.getElementById("name");
+      var cID = document.getElementById("cID");
+      var email = document.getElementById("sEMAIL")
+    }
+})();
