87a5cfb17fc0c28db888589fdae8feeb4e6955bb
dixon <dixontir@cs.washington.edu>
2020-10-18 09:26:19 +0700

    added parallel processing in groups

--- utils/groups.js
+++ utils/groups.js
@@ -1,26 +1,51 @@
-async function quickstart() {
-    // Imports the Google Cloud client library
-    const language = require('@google-cloud/language');
-  
-    // Instantiates a client
-    const client = new language.LanguageServiceClient();
-  
-    // The text to analyze
-    const text = 'Hello, world!';
-  
-    const document = {
-      content: text,
-      type: 'PLAIN_TEXT',
-    };
-  
-    // Detects the sentiment of the text
-    const [result] = await client.analyzeSentiment({document: document});
-    const sentiment = result.documentSentiment;
-  
-    console.log(`Text: ${text}`);
-    console.log(`Sentiment score: ${sentiment.score}`);
-    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-  }
-  
+const language = require('@google-cloud/language');
+const client = new language.LanguageServiceClient();
+const Q = require('q');  
 
-quickstart();
+async function analyzeText(text) {
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  };
+
+  // Detects the sentiment of the document
+  const [result] = await client.analyzeSentiment({document});
+
+  const sentiment = result.documentSentiment;
+  console.log('Document sentiment:');
+  console.log(text);
+  console.log(`  Score: ${sentiment.score}`);
+  console.log(`  Magnitude: ${sentiment.magnitude}`);
+  return sentiment;
+
+
+  // const sentences = result.sentences;
+  // sentences.forEach(sentence => {
+  //   console.log(`Sentence: ${sentence.text.content}`);
+  //   console.log(`  Score: ${sentence.sentiment.score}`);
+  //   console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
+  // });
+}
+
+async function analyzeUsers(users, sentimentTemp) {
+  const promises = [];
+  for (let i = 0; i < users.length; i++) {
+    // let id = users[i].sID;
+    // sentiments[id] = analyzeText(users[i].responses);
+    sentimentTemp[i] = analyzeText(users[i]);
+    promises.push(sentimentTemp[i]);
+  }
+  return sentimentTemp;
+}
+
+await function formGroups(users, maxGroup) {
+  var trial = ["I love ice cream", "asdqwdn", "I hate this hw", "Is it raining now?"];
+  var sentiments = []
+  analyzeUsers(trial, sentiments);
+  Q.all(sentiments).then(function() {
+    console.log(sentiments);
+    return groups;
+  });
+}
+
+formGroups();
